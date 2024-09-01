751711456cb664874d758712c02b10a282f03a8c
Nycthos <nycthos@nycthos.duckdns.org>
2024-09-01 06:16:58 +0200

    init

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_TB372FC-user
+add_lunch_combo omni_TB372FC-userdebug
+add_lunch_combo omni_TB372FC-eng
